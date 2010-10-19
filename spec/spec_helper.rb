72d0cbb17dcb3f31e03f9c5ce1c487e4d386941a
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2010-10-18 23:15:40 -0200

    Make specs pass in ruby 1.9
    . is no longer in ruby's  since 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rubygems'
 require 'postmark'
-require 'lib/postmark-rails'
+require 'postmark-rails'
 require 'spec'
 require 'spec/autorun'
 
