f90e283c5cceb69e893a6addc34b6fe56e616e8c
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-05-10 22:17:43 +0800

    Add TestMailer#message_with_attachments.

--- spec/fixtures/models/test_mailer.rb
+++ spec/fixtures/models/test_mailer.rb
@@ -1,8 +1,8 @@
 class TestMailer < ActionMailer::Base
-  default :subject  => 'hello', 
-          :to       => 'sheldon@bigbangtheory.com', 
+  default :subject  => 'hello',
+          :to       => 'sheldon@bigbangtheory.com',
           :from     => 'leonard@bigbangtheory.com'
-          
+
   def simple_message
     mail
   end
@@ -16,5 +16,10 @@ class TestMailer < ActionMailer::Base
       format.text
       format.html
     end
+  end
+
+  def message_with_attachment
+    attachments['empty.gif'] = File.join(File.dirname(__FILE__), '..', '..', 'fixtures', 'empty.gif')
+    mail(:subject => "Message with attachment.")
   end
 end
