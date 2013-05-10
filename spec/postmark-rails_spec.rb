29a4c1dfab92b3183ad554cd14126cc67ceacb2f
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-05-10 22:08:08 +0800

    Fix unit tests.

--- spec/postmark-rails_spec.rb
+++ spec/postmark-rails_spec.rb
@@ -1,30 +1,32 @@
 require 'spec_helper'
 
 describe "PostmarkRails3" do
+  let!(:api_client) { Postmark::ApiClient.new('api-key') }
+
   it "should allow setting an api key" do
-    Postmark.stub!(:send_through_postmark)
     ActionMailer::Base.postmark_settings = {:api_key => 'api-key'}
     ActionMailer::Base.postmark_settings[:api_key].should == 'api-key'
-    Postmark.should_receive(:api_key=).with('api-key')
-    TestMailer.simple_message.deliver
   end
-  
+
   it "should use postmark for delivery" do
-    Postmark.should_receive(:send_through_postmark) do |message|
+    Postmark::ApiClient.should_receive(:new) { api_client }
+    api_client.should_receive(:deliver_message) do |message|
       message.subject.should == "hello"
     end
     TestMailer.simple_message.deliver
   end
 
   it "should allow tagging of message" do
-    Postmark.should_receive(:send_through_postmark) do |message|
+    Postmark::ApiClient.should_receive(:new) { api_client }
+    api_client.should_receive(:deliver_message) do |message|
       message.tag.to_s.should == "delivery"
     end
     TestMailer.tagged_message.deliver
   end
-  
+
   it "should work with multipart messages" do
-    Postmark.should_receive(:send_through_postmark) do |message|
+    Postmark::ApiClient.should_receive(:new) { api_client }
+    api_client.should_receive(:deliver_message) do |message|
         message.should be_multipart
         message.body_text.strip.should == "hello"
         message.body_html.strip.should == "<b>hello</b>"
