6fa901d9dc312e8e80c20fec624564750473aeda
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-05-10 21:51:25 +0800

    Remove attachments fix from the Rails 3 installation process.

--- lib/postmark-rails.rb
+++ lib/postmark-rails.rb
@@ -4,7 +4,7 @@ require 'postmark_delivery_method'
 
 module PostmarkInstaller
   extend self
-  
+
   def auto_detect_and_install
     if ActionMailer::Base.respond_to?(:add_delivery_method)
       install_in_rails_3
@@ -12,16 +12,15 @@ module PostmarkInstaller
       install_in_rails_2
     end
   end
-  
+
   def install_in_rails_2
     ActionMailer::Base.send(:include, PostmarkDeliveryMethod)
   end
-  
+
   def install_in_rails_3
     ActionMailer::Base.add_delivery_method :postmark, Mail::Postmark, :api_key => nil
-    Mail::Message.send(:include, Postmark::AttachmentsFixForMail)
   end
-  
+
 end
 
 PostmarkInstaller.auto_detect_and_install
