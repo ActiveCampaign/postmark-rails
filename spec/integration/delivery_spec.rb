839e214f04f298edcde03f29031c0d1eef9be00f
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-05-10 22:10:10 +0800

    Add basic integration tests.

---
+++ spec/integration/delivery_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe "Delivering messages with postmark-rails" do
+  let(:api_key) { 'POSTMARK_API_TEST' }
+
+  before do
+    ActionMailer::Base.postmark_settings = { :api_key => api_key }
+  end
+
+  it 'delivers a simple message' do
+    message = TestMailer.simple_message
+
+    expect { message.deliver }.to change{message.delivered?}.to(true)
+  end
+
+  it 'delivers a tagged message' do
+    message = TestMailer.tagged_message
+
+    expect { message.deliver }.to change{message.delivered?}.to(true)
+  end
+
+  it 'delivers a multipart message' do
+    message = TestMailer.multipart_message
+
+    expect { message.deliver }.to change{message.delivered?}.to(true)
+  end
+end
