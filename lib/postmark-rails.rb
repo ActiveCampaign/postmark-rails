de428d3b6ffcf3b2753c45fc420603600e740fae
Ben Woosley <ben.woosley@gmail.com>
2013-08-15 03:26:46 -0700

    If rails is defined, use a railtie, so initialization will be tidy.

--- lib/postmark-rails.rb
+++ lib/postmark-rails.rb
@@ -4,9 +4,13 @@ require 'postmark'
 module PostmarkRails
   extend self
 
-  def install_in_rails
+  def install
     ActionMailer::Base.add_delivery_method :postmark, Mail::Postmark, :api_key => nil
   end
 end
 
-PostmarkRails.install_in_rails
+if defined?(Rails)
+  require 'postmark-rails/railtie'
+else
+  PostmarkRails.install
+end
