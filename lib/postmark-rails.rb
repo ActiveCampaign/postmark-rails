046620e3c90088f9a3f111270977d11549b10d37
Ben Woosley <ben.woosley@gmail.com>
2013-08-15 03:26:04 -0700

    Since the readme itself says we don't support rails < 3, drop the old PostmarkRails::DeliveryMethod module.

--- lib/postmark-rails.rb
+++ lib/postmark-rails.rb
@@ -2,26 +2,11 @@ require 'action_mailer'
 require 'postmark'
 
 module PostmarkRails
-  extend ActiveSupport::Autoload
   extend self
-
-  autoload :DeliveryMethod, 'postmark-rails/delivery_method'
-
-  def auto_detect_and_install
-    if ActionMailer::Base.respond_to?(:add_delivery_method)
-      install_in_rails
-    else
-      install_in_legacy_rails
-    end
-  end
-
-  def install_in_legacy_rails
-    ActionMailer::Base.send(:include, PostmarkRails::DeliveryMethod)
-  end
 
   def install_in_rails
     ActionMailer::Base.add_delivery_method :postmark, Mail::Postmark, :api_key => nil
   end
 end
 
-PostmarkRails.auto_detect_and_install
+PostmarkRails.install_in_rails
