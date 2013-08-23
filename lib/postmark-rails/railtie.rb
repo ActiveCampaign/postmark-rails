6c98f4b917bc77aac8f7187549e90a550a3f50e0
Artem Chistyakov <chistyakov.artem@gmail.com>
2013-08-23 14:37:54 +0800

    Configure the Railtie initializer to be executed before ActionMailer’s action_mailer.set_configs. This will allow users to use the gem settings in application.rb. #37

--- lib/postmark-rails/railtie.rb
+++ lib/postmark-rails/railtie.rb
@@ -1,6 +1,6 @@
 module PostmarkRails
   class Railtie < Rails::Railtie
-    initializer 'postmark-rails' do
+    initializer 'postmark-rails', :before => 'action_mailer.set_configs' do
       ActiveSupport.on_load :action_mailer do
         PostmarkRails.install
       end
